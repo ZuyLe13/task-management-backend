a90a9dab081c551fd9a47051052c7267e9cfab64
dile <duyle131103@gmail.com>
2025-08-06 17:48:06 +0700

    feat: implement update, delete task type API

--- src/models/task-type.model.ts
+++ src/models/task-type.model.ts
@@ -3,18 +3,18 @@ import mongoose, { Schema } from "mongoose";
 export interface TaskType {
   name: String;
   code: String;
-  icon?: String;
+  icon: String;
   isActive: Boolean;
   isSubTask: Boolean;
 }
 
-export const taskTypeSchema = new Schema<TaskType>(
+const taskTypeSchema = new Schema<TaskType>(
   {
     name: { type: String, required: true },
     code: { type: String, required: true },
     icon: { type: String, required: true },
-    isActive: { type: Boolean, required: true, default: true },
-    isSubTask: { type: Boolean, default: false }
+    isActive: { type: Boolean },
+    isSubTask: { type: Boolean }
   },
   { timestamps: true }
 );
--- src/schemas/task-type.schema.ts
+++ src/schemas/task-type.schema.ts
@@ -7,17 +7,13 @@ export const taskTypeValidationSchema = Joi.object({
     'string.min': 'Task type name must be at least 2 character long',
     'string.max': 'Task type name must not exceed 100 characters'
   }),
-  code: Joi.string().trim().optional().min(2).max(100).uppercase().pattern(/^[A-Z_]+$/).messages({
-    'string.min': 'Task type code must be at least 2 character long',
-    'string.max': 'Task type code must not exceed 100 characters',
-  }),
   icon: Joi.string().trim().optional().allow('').messages({
     'string.base': 'Icon must be a string'
   }),
-  isActive: Joi.boolean().default(true).messages({
+  isActive: Joi.boolean().optional().default(true).messages({
     'boolean.base': 'isActive must be a boolean'
   }),
-  isSubTask: Joi.boolean().default(false).messages({
+  isSubTask: Joi.boolean().optional().default(false).messages({
     'boolean.base': 'isSubTask must be a boolean'
   })
 });
